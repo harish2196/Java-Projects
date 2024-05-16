8bb3e1e62470e93251b80f913859199c132c0cd4
Hari3552 <Hari3552@CSL0695.global.chainsys.com>
2024-05-16 10:55:44 +0530

    Servlet SignUp Form - ArrayList

--- DemoServlet/src/main/java/com/chainsys/servlet/DemoServ.java
+++ DemoServlet/src/main/java/com/chainsys/servlet/DemoServ.java
@@ -2,12 +2,15 @@ package com.chainsys.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class DemoServ
@@ -31,11 +34,27 @@ public class DemoServ extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("get");
-		String name=request.getParameter("name");
-		int age=Integer.parseInt(request.getParameter("age"));
+		String name=request.getParameter("username");
+		String email=request.getParameter("email");
+		String dob = request.getParameter("date");
+		//String gender=request.getParameter("gender");
+		String pass=request.getParameter("password");
+		//int phoneNo=Integer.parseInt(request.getParameter("age"));
 		PrintWriter out=response.getWriter();
 		out.println(name);
+		out.println(email);
+		out.println(dob);
+		//out.println(gender);
+		out.println(pass);
+		ArrayList<String> form= new ArrayList<String>();
+		form.add(name);
+		form.add(email);
+		form.add(dob);
+		//form.add(gender);
+		form.add(pass);
+		System.out.println(form);
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
